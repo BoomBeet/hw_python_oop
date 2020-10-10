6c0a75d0056d1b3540490037eca9ab59166ec2de
BoomBeet <vladimir21beloglazov@gmail.com>
2020-10-10 12:58:15 +0300

    almost done
    Everything works except for cashcalc

--- homework.py
+++ homework.py
@@ -9,44 +9,46 @@ properdatenow = now.date()
 nowstring = f'{now.day}.{now.month}.{now.year}'
 
 
-records = []
-total = 0
-
-for record in records:
-    if record.date == properdatenow:
-        total += record.amount
 
 class Calculator:
-    def __init__(self, limit, total):
+    def __init__(self, limit):
         self.limit = limit
-        self.total = total
+        self.records = []
+        self.total = 0
 
     def add_record(self, recording):
-        records.append(recording)
+        global properdatenow
+        self.recording = recording
+        self.records.append(recording)
+        if recording.date == properdatenow:
+            self.total += recording.amount
+
+    def today_remainder(self):
+        return self.limit - self.total
 
     def get_today_stats(self):
         return self.total
 
     def get_week_stats(self):
-        week = range((properdatenow - dt.timedelta(days=7)), (properdatenow + dt.timedelta(days=1)))
-        weektotal = 0
-        for record in records:
-            if record.date in week:
-                weektotal += record.amount
-        return weektotal
+        global properdatenow
+        weekstart = properdatenow - dt.timedelta(days=7)
+        return sum([
+            record.amount
+            for record in self.records
+            if weekstart < record.date <= properdatenow
+        ])
 
 
 class CashCalculator(Calculator):
     def __init__(self, limit):
-        super().__init__(limit, total)
-
-    RUB_RATE = float(1)
-    USD_RATE = float(70)
-    EUR_RATE = float(80)
+        super().__init__(limit)
+        RUB_RATE = float(1)
+        USD_RATE = float(80)
+        EUR_RATE = float(90)
 
     def get_today_cash_remained(self, currency):
         self.currency = currency
-        remainder = float(self.limit - self.total)
+        remainder = self.today_remainder()
         if remainder > 0:
             if currency == 'rub':
                 value = truncate(remainder / self.RUB_RATE, 2)
@@ -57,11 +59,9 @@ class CashCalculator(Calculator):
             elif currency == 'eur':
                 value = truncate(remainder / self.EUR_RATE, 2)
                 return f'На сегодня осталось {value} Euro'
-
-        if remainder == 0:
+        elif remainder == 0:
             return f'Денег нет, держись'
-
-        if remainder < 0:
+        else:
             if currency == 'rub':
                 value = truncate(abs(remainder) / self.RUB_RATE, 2)
                 return f'Денег нет, держись: твой долг - {value} руб'
@@ -75,11 +75,11 @@ class CashCalculator(Calculator):
 
 class CaloriesCalculator(Calculator):
     def __init__(self, limit):
-        super().__init__(limit, total)
+        super().__init__(limit)
 
     def get_calories_remained(self):
-        remainder = float(self.limit - self.total)
-        if remainder > 0:
+        remainder = self.today_remainder()
+        if self.today_remainder() > 0:
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {remainder} кКал'
         else:
             return f'Хватит есть!'
@@ -91,21 +91,3 @@ class Record:
         self.amount = amount
         self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
         self.comment = comment
-    def __str__(self):
-        return str(self.amount) + self.comment + str(self.date)
-
-
-# создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
-        
-# дата в параметрах не указана, 
-# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-# и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-# а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-                
-print(cash_calculator.get_today_cash_remained("rub"))
-print(*records)
-print(total)
